cb5302ea98cd517056bea29b1b34f37da9c7e658
cesar souza <cesarfernandes@macbook-pro-de-cesar.local>
2019-01-24 21:13:04 -0200

    Initial Commit

---
+++ app-ratefood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-ratefood
+//
+//  Created by cesar souza on 24/01/19.
+//  Copyright © 2019 cesar souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
